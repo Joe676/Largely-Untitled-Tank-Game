4310df4fcae4c951d022cf5dc6dd9560fd5d28ca
Józek <bossowski92@gmail.com>
2022-12-11 17:03:00 +0100

    Level cleanup

--- Godot Files/Levels/Level.gd
+++ Godot Files/Levels/Level.gd
@@ -10,7 +10,6 @@ onready var spawn_points = [
 ]
 
 onready var camera_target = $CameraTarget
-var is_hooked = false
 
 const camera_target_displaycement = Vector3(0, 6, -10)
 
@@ -20,23 +19,18 @@ func _ready():
 	Global.move_players_to_spawnpoints(spawn_points)
 
 func _physics_process(_delta):
-	if not is_hooked:
-		hook_camera_to_player()
+	hook_camera_to_player()
 	
 
 func hook_camera_to_player():
 	var current_player: KinematicBody = PersistentNodes.get_node(str(get_tree().get_network_unique_id()))
 	if current_player == null:
 		return
-	current_player.add_child(camera_target)
+
+	var player_position = current_player.global_translation
+	
+	camera_target.global_transform = current_player.global_transform
 	camera_target.translate(camera_target_displaycement)
-	is_hooked = true
-
-
-	# var player_position = current_player.global_translation
 	
-	# camera_target.global_transform = current_player.global_transform
-	# camera_target.translate(camera_target_displaycement)
-	
-	# camera_target.look_at(player_position, Vector3.UP)
+	camera_target.look_at(player_position, Vector3.UP)
 
